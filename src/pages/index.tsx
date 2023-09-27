ab2062776ca294cb473e2bececee6928e7fba242
Jaival <pateljaival05@gmail.com>
2023-09-27 09:17:44 -0400

    css prod changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,10 +108,10 @@ export default function Home() {
           
               </div>
               <div style={{width: '600px', lineHeight:'40px', display: 'inline-block', marginTop: '20px'}}>
-                  <Text fontFamily={'sans-serif'} fontSize={20}>FireWallet`&apos;` Partioning Protocol generates hidden sub-wallets connected to the main wallet and employs a partitioning tree algorithm to distribute tokens randomly among wallets, giving the appearance that they all hold <Text as='b'>{'<'}1 ETH</Text>.</Text>
+                  <Text fontFamily={'sans-serif'} fontSize={20}>FireWallet&apos; Partioning Protocol generates hidden sub-wallets connected to the main wallet and employs a partitioning tree algorithm to distribute tokens randomly among wallets, giving the appearance that they all hold <Text as='b'>{'<'}1 ETH</Text>.</Text>
               </div>
               <div style={{width: '600px', lineHeight:'40px', display: 'inline-block', marginTop: '20px'}}>
-                <Text fontFamily={'sans-serif'} fontSize={20}>This approach discourages attackers, as each wallet seems to contain only a minimal amount of ETH, making it an unattractive target for depletion. Even if an attacker does target one of user`&apos;` the wallets, only a small fraction of the original tokens sent through the algorithm is at risk, safeguarding <Text as='b'>97%</Text> of the user`&apos;` original tokens.</Text>
+                <Text fontFamily={'sans-serif'} fontSize={20}>This approach discourages attackers, as each wallet seems to contain only a minimal amount of ETH, making it an unattractive target for depletion. Even if an attacker does target one of user&apos; the wallets, only a small fraction of the original tokens sent through the algorithm is at risk, safeguarding <Text as='b'>97%</Text> of the user&apos; original tokens.</Text>
               </div>
 
             </div>
@@ -170,13 +170,13 @@ export default function Home() {
             </Center>
             <Center>
               <div style={{padding: '10px', color:'#6D6875', marginBottom: '20px'}}>
-                <Text as='b' fontFamily={'sans-serif'} fontSize={18}>Note: `&quot;`Amount in USD`&quot;` conversion feature is now live :) as upon Sept 4th 12:00 AM EST</Text>
+                <Text as='b' fontFamily={'sans-serif'} fontSize={18}>Note: &quot;Amount in USD&quot; conversion feature is now live :) as upon Sept 4th 12:00 AM EST</Text>
               </div>
             </Center>
             <Center>
               <div style={{height:'600px', width:'1300px', border: '2px solid black'}}>
                 <div style={{position: "relative", paddingBottom: "64.63195691202873%", height: "0"}}>
-                  <iframe src="https://www.loom.com/embed/6d0cf87793e7448280fc9fed499590ef?sid=00e2e944-7229-40a6-9c14-17dca0e9cb56" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style={{position: "absolute", top: '0', left: "0", width: "1300px", height: "600px"}}></iframe></div>
+                  <iframe src="https://www.loom.com/embed/6d0cf87793e7448280fc9fed499590ef?sid=00e2e944-7229-40a6-9c14-17dca0e9cb56" frameBorder="0"  style={{position: "absolute", top: '0', left: "0", width: "1300px", height: "600px"}}></iframe></div>
               </div>
             </Center>
           </div>
@@ -205,7 +205,7 @@ export default function Home() {
                 <Text as='b' fontFamily={'sans-serif'} fontSize={40}>Check out the Docs to learn everything about FireWallet.</Text>
               </div>
               <div style={{marginTop:'15px', textAlign: 'left'}}>
-                <Text fontFamily={'sans-serif'} fontSize={20}>From learning about FireWallet`&apos;`s Partition Protocols to all its Features, the docs has an answer for all - The docs also links tutorials, resources, and bug fixes that the user may run into. </Text>
+                <Text fontFamily={'sans-serif'} fontSize={20}>From learning about FireWallet&apos;s Partition Protocols to all its Features, the docs has an answer for all - The docs also links tutorials, resources, and bug fixes that the user may run into. </Text>
               </div>
               <Center>
                 <div style={{marginTop: '30px'}}>
